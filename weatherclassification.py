e9f182600905b0153ba7f1f9f65c6f091b46e1e4
Ataaaan <134145248+nathaniel1102@users.noreply.github.com>
2023-10-15 22:26:55 +0800

    Update weatherclassification.py

--- weatherclassification.py
+++ weatherclassification.py
@@ -1,31 +1,34 @@
 import streamlit as st
 import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('weatherclass.h5')
-  return model
-model=load_model()
-st.write("""# Weather Classification"""
-)
-file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
+    model = tf.keras.models.load_model('weatherclass.h5')
+    return model
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(224,224)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+model = load_model()
+st.write("# Weather Classification")
+
+file = st.file_uploader("Choose weather photo from computer", type=["jpg", "png"])
+
+def import_and_predict(image_data, model):
+    size = (224, 224)
+    image = Image.open(image_data)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Cloudy', 'Rain', 'Sunrise', 'Shine']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_names = ['Cloudy', 'Rain', 'Sunrise', 'Shine']
+    string = "OUTPUT: " + class_names[np.argmax(prediction)]
     st.success(string)
